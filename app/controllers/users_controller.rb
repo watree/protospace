af26ede82c85930b228aa2c23f9626f2df84ca44
wakimoto izumi <Izumi@wakimoto-no-MacBook-Air.local>
2016-05-20 10:18:04 +0900

    add users controller actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,16 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+
+  def edit
+  end
+
+  def update
+    current_user.update(update_params)
+  end
+
+  private
+  def update_params
+   params.require(:user).permit(:nickname, :avatar, :profile, :member, :works)
+  end
 end
