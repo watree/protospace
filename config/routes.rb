a25ba2feec0fd9be67950c30ae9f6067b962d3b8
wakimoto izumi <Izumi@wakimoto-no-MacBook-Air.local>
2016-05-27 16:27:15 +0900

    add route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root 'prototypes#index'
 
-  resources :prototypes, only: [:index, :new, :show]
+  resources :prototypes, only: [:index, :new, :show, :create]
   resources :users, only: [:show, :edit, :update]
 end
