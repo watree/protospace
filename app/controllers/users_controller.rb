c5c124dbf502d3f319effe50d4388172e03f4d95
wakimoto izumi <Izumi@wakimoto-no-MacBook-Air.local>
2016-05-27 11:45:29 +0900

    rewrite users#update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,8 +7,7 @@ class UsersController < ApplicationController
   end
 
   def update
-    current_user.update(update_params)
-    if current_user.update
+    if current_user.update(update_params)
       redirect_to root_path, notice: 'Updated successfully'
     else
       render :edit
