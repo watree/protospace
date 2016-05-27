18d6c1eadfe6178a1a12641a617968844a6280c7
wakimoto izumi <Izumi@wakimoto-no-MacBook-Air.local>
2016-05-27 14:49:32 +0900

    create prototypes table

--- db/migrate/20160527044622_create_prototypes.rb
+++ db/migrate/20160527044622_create_prototypes.rb
@@ -1,8 +1,11 @@
 class CreatePrototypes < ActiveRecord::Migration
   def change
     create_table :prototypes do |t|
-
-      t.timestamps null: false
+      t.string :title
+      t.string :catch_copy
+      t.text :concept
+      t.references :user, index: true, foreign_key: true
+      t.timestamps
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160515034432) do
+ActiveRecord::Schema.define(version: 20160527044622) do
+
+  create_table "prototypes", force: :cascade do |t|
+    t.string   "title",      limit: 255
+    t.string   "catch_copy", limit: 255
+    t.text     "concept",    limit: 65535
+    t.integer  "user_id",    limit: 4
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "prototypes", ["user_id"], name: "index_prototypes_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  limit: 255,   default: "", null: false
@@ -35,4 +46,5 @@ ActiveRecord::Schema.define(version: 20160515034432) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "prototypes", "users"
 end
