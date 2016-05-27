c51db1abb7fe39f373d8b953666cef23fe1caba5
wakimoto izumi <Izumi@wakimoto-no-MacBook-Air.local>
2016-05-27 15:57:23 +0900

    define associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  has_many :prototypes
+
   validates :nickname, presence: true
 end
