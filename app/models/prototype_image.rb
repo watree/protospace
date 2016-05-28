36234417b3f363511eb86fc83960842454ce83b2
wakimoto izumi <Izumi@wakimoto-no-MacBook-Air.local>
2016-05-28 16:57:52 +0900

    correct miss typo

--- app/models/prototype_image.rb
+++ app/models/prototype_image.rb
@@ -1,6 +1,6 @@
 class PrototypeImage < ActiveRecord::Base
   mount_uploader :image, PrototypeImageUploader
   belongs_to :prototype
-  emun type: %i(main sub)
+  enum type: %i(main sub)
   validates :image, :type, presence: true
 end
