30ff35c17156e5899981fe0cf81f37d394d7ad6c
wakimoto izumi <Izumi@wakimoto-no-MacBook-Air.local>
2016-05-28 12:31:22 +0900

    set uploader for prototype_image

--- app/models/prototype_image.rb
+++ app/models/prototype_image.rb
@@ -1,4 +1,5 @@
 class PrototypeImage < ActiveRecord::Base
+  mount_uploader :image, PrototypeImageUploader
   belongs_to :prototype
   emun type: %i(main sub)
   validates :image, :type, presence: true
