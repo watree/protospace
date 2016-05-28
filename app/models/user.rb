25122a6123b6f6e9b0b0d7d3a6c1e6022af28262
wakimoto izumi <Izumi@wakimoto-no-MacBook-Air.local>
2016-05-28 12:39:20 +0900

    set uploader for avatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  mount_uploader :AvatarUploader
+
   has_many :prototypes
 
   validates :nickname, presence: true
