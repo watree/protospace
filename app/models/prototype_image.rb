c716ee0668f7c78872ab581638214db53a6f7e66
wakimoto izumi <Izumi@wakimoto-no-MacBook-Air.local>
2016-05-29 01:00:16 +0900

    edit image_model to use column'type'

--- app/models/prototype_image.rb
+++ app/models/prototype_image.rb
@@ -2,5 +2,6 @@ class PrototypeImage < ActiveRecord::Base
   mount_uploader :image, PrototypeImageUploader
   belongs_to :prototype
   enum type: %i(main sub)
+  self.inheritance_column = :_type_disabled
   validates :image, :type, presence: true
 end
