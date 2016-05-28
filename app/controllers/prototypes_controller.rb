257774b37fa1e4b75a6eb22c3e6d82866cec6306
wakimoto izumi <Izumi@wakimoto-no-MacBook-Air.local>
2016-05-28 10:11:34 +0900

    edit prototypes controller

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,2 +1,26 @@
 class PrototypesController < ApplicationController
+  def new
+    @prototype = Prototype.new
+    @prototype.prototype_images.build
+  end
+
+  def create
+    @prototype = Prototype.new(prototype_params)
+    if @prototype.save
+      redirect_to root_path, notice: 'Created successfully'
+    else
+      render :new
+    end
+  end
+
+  private
+  def prototype_params
+    params.require(:prototype).permit(
+      :title,
+      :catch_copy,
+      :concept,
+      :user_id
+      prototype_images_attributes: [:image, :type]
+    )
+  end
 end
