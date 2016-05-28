c8cc0d6147bf34c504e32f9477aace12008aabec
wakimoto izumi <Izumi@wakimoto-no-MacBook-Air.local>
2016-05-29 02:33:44 +0900

    nest routes to get user_id parameter

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'prototypes#index'
 
-  resources :prototypes, only: [:index, :new, :show, :create]
-  resources :users, only: [:show, :edit, :update]
+  resources :users, only: [:show, :edit, :update] do
+    resources :prototypes, only: [:index, :new, :show, :create]
+  end
 end
