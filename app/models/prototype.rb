75ab9b9908ca63d77326415a6b703b3093bbab67
wakimoto izumi <Izumi@wakimoto-no-MacBook-Air.local>
2016-06-07 23:53:45 +0900

    add relations to models

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -1,6 +1,7 @@
 class Prototype < ActiveRecord::Base
   belongs_to :user
   has_many :prototype_images, dependent: :delete_all
+  has_many :likes, dependent: :destroy
   accepts_nested_attributes_for :prototype_images
   validates :catch_copy, :concept, :title, presence: true
 end
