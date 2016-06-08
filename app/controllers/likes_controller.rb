6c184347dbf46677ba941eb31572558957853614
wakimoto izumi <Izumi@wakimoto-no-MacBook-Air.local>
2016-06-08 13:12:24 +0900

    create likes controller

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,2 +1,19 @@
 class LikesController < ApplicationController
+  before_action :set_prototype
+
+  def create
+    @like = @prototype.likes.create(user_id: current_user.id)
+    @likes = @prototype.likes
+  end
+
+  def destroy
+    like = @prototype.likes.find_by(user_id: current_user.id)
+    like.destroy
+    @likes = @prototype.likes
+  end
+
+  private
+  def set_prototype
+    @prototype = Prototype.find(params[:prototype_id])
+  end
 end
