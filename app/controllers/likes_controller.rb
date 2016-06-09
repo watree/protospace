16e882561624cbab43ad55bc597e70332c2c1626
wakimoto izumi <Izumi@wakimoto-no-MacBook-Air.local>
2016-06-09 11:51:40 +0900

    add blank

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -7,10 +7,10 @@ class LikesController < ApplicationController
 
   def destroy
     @prototype.likes.find_by(user_id: current_user.id).destroy
-    @prototype = Prototype.find(params[:prototype_id])
   end
 
   private
+
   def set_prototype
     @prototype = Prototype.find(params[:prototype_id])
   end
