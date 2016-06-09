8fe0b5bad85b5c73ee925f88197f243fc5b55bcb
wakimoto izumi <Izumi@wakimoto-no-MacBook-Air.local>
2016-06-09 10:26:29 +0900

    rewrite likes controller

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,14 +2,12 @@ class LikesController < ApplicationController
   before_action :set_prototype
 
   def create
-    @like = @prototype.likes.create(user_id: current_user.id)
-    @likes = @prototype.likes
+    @prototype.likes.create(user_id: current_user.id)
   end
 
   def destroy
-    like = @prototype.likes.find_by(user_id: current_user.id)
-    like.destroy
-    @likes = @prototype.likes
+    @prototype.likes.find_by(user_id: current_user.id).destroy
+    @prototype = Prototype.find(params[:prototype_id])
   end
 
   private
