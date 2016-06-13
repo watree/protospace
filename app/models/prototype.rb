0b99019f8bc0c03b1623e7501d5cf2a6260d0fb2
wakimoto izumi <Izumi@wakimoto-no-MacBook-Air.local>
2016-06-13 16:47:00 +0900

    set associations of comments

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -2,6 +2,7 @@ class Prototype < ActiveRecord::Base
   belongs_to :user
   has_many :prototype_images, dependent: :delete_all
   has_many :likes, dependent: :destroy
+  has_many :comments, dependent: :destroy
   accepts_nested_attributes_for :prototype_images
   validates :catch_copy, :concept, :title, presence: true
 
