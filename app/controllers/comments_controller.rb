c01659f42258906ec5b81fba885f6ae1cb16f367
wakimoto izumi <Izumi@wakimoto-no-MacBook-Air.local>
2016-06-15 19:06:23 +0900

    generate comment controller

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,17 @@
+class CommentsController < ApplicationController
+before_action :set_prototype, only: :create
+  def create
+    @prototype.comments.create(comments_params)
+    @comments = @prototype.comments
+  end
+
+  private
+
+  def set_prototype
+    @prototype = Prototype.find(params[:prototype_id])
+  end
+
+  def comments_params
+    params.require(:comment).permit(:prototype_id, :user_id, :text)
+  end
+end
