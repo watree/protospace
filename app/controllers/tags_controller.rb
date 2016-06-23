9021276995692886a161131b66976d6c11e95b66
wakimoto izumi <Izumi@wakimoto-no-MacBook-Air.local>
2016-06-23 16:59:58 +0900

    add actions of tag controller

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,8 +1,16 @@
 class TagsController < ApplicationController
 
   def index
+    @tags = ActsAsTaggableOn::Tag.all
   end
 
   def show
+    @tag = ActsAsTaggableOn::Tag.find(params[:id])
+    @prototypes = Prototype \
+                    .tagged_with(@tag)
+                    .includes(:user)
+                    .order(created_at: :DESC)
+                    .page(params[:page])
+                    .per(8)
   end
 end
